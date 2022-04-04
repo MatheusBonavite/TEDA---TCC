f9461cd0f7097a793ba2c6a698624d3a0d00fd5d
MatheusBonavite <matheusbonavite@ufmg.br>
2022-04-04 20:49:09 -0300

    TST002: successfully testing euclidean distance

--- test_env/EuclideanDistance/EuclideanDistance.test.cpp
+++ test_env/EuclideanDistance/EuclideanDistance.test.cpp
@@ -3,17 +3,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-double euclidean_distance(double **matrix, unsigned int i, unsigned int j, unsigned int amount_of_columns)
-{
-    double result = 0.0;
-    for (int w = 0; w < amount_of_columns; w++)
-    {
-        result += ((matrix[i][w] - matrix[j][w]) * (matrix[i][w] - matrix[j][w]));
-    }
-
-    return (double)result;
-}
-
 void zero_matrix_initializer(double **matrix, unsigned int rows, unsigned int cols)
 {
     for (int i = 0; i < rows; i++)
@@ -35,18 +24,72 @@ double **matrix_allocation(unsigned int rows, unsigned int columns)
     return matrix;
 }
 
-TEST_CASE("This is a test")
+void dealloc_matrix(double **matrix, unsigned int rows)
 {
-
-    unsigned int rows = 14;
-    unsigned int columns = 1;
-
-    double **matrix = matrix_allocation(rows, columns);
-
-    REQUIRE(2 + 2 == 4);
     for (int i = 0; i < rows; i++)
     {
         free(matrix[i]);
     }
     free(matrix);
+}
+
+double euclidean_distance(double **matrix, unsigned int i, unsigned int j, unsigned int amount_of_columns)
+{
+    double result = 0.0;
+    for (int w = 0; w < amount_of_columns; w++)
+    {
+        result += ((matrix[i][w] - matrix[j][w]) * (matrix[i][w] - matrix[j][w]));
+    }
+
+    return (double) sqrt(result);
+}
+
+double squared_euclidean_distance(double **matrix, unsigned int i, unsigned int j, unsigned int amount_of_columns)
+{
+    double result = 0.0;
+    for (int w = 0; w < amount_of_columns; w++)
+    {
+        result += ((matrix[i][w] - matrix[j][w]) * (matrix[i][w] - matrix[j][w]));
+    }
+
+    return (double) result;
+}
+
+TEST_CASE("One dimensional euclidean distance")
+{
+
+    unsigned int rows = 14;
+    unsigned int columns = 1;
+    double test_1d[] = {
+        20.2, 3.0, 6.4, 11.6, 8.2, 2.2, 11.2, 5.2, 6.2, 0.2, 1.0, 4.8, 2.4, 3.8
+    };
+    double **matrix = matrix_allocation(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            matrix[i][j] = test_1d[i];
+        }
+    }
+    REQUIRE(
+        euclidean_distance(matrix, 0, 1, 1) == 17.2
+    );
+    dealloc_matrix(matrix, rows);
+}
+
+TEST_CASE("One dimensional squared euclidean distance")
+{
+    unsigned int rows = 14;
+    unsigned int columns = 1;
+    double test_1d[] = {
+        20.2, 3.0, 6.4, 11.6, 8.2, 2.2, 11.2, 5.2, 6.2, 0.2, 1.0, 4.8, 2.4, 3.8
+    };
+    double **matrix = matrix_allocation(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            matrix[i][j] = test_1d[i];
+        }
+    }
+    REQUIRE(
+        squared_euclidean_distance(matrix, 0, 1, 1) == 295.84
+    );
+    dealloc_matrix(matrix, rows);
 }
--- test_env/EuclideanDistance/build.test.sh
+++ test_env/EuclideanDistance/build.test.sh
@@ -1,1 +1,2 @@
 g++ -std=c++11 EuclideanDistance.test.cpp -o EuclideanTest
+valgrind --log-file="euclidean-report" --leak-check="full" ./EuclideanTest
