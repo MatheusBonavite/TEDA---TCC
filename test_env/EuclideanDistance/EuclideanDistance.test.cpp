695fac357b136ffcbbe1817aec5c076eb120c345
Matheus Bonavite <matheusbonavite@gmail.com>
2022-04-04 18:39:25 -0300

    TST001: adding euclidean distance test

--- test_env/EuclideanDistance/EuclideanDistance.test.cpp
+++ test_env/EuclideanDistance/EuclideanDistance.test.cpp
@@ -1,7 +1,52 @@
 #define CATCH_CONFIG_MAIN
 #include "../catch.hh"
+#include <math.h>
+#include <stdlib.h>
+
+double euclidean_distance(double **matrix, unsigned int i, unsigned int j, unsigned int amount_of_columns)
+{
+    double result = 0.0;
+    for (int w = 0; w < amount_of_columns; w++)
+    {
+        result += ((matrix[i][w] - matrix[j][w]) * (matrix[i][w] - matrix[j][w]));
+    }
+
+    return (double)result;
+}
+
+void zero_matrix_initializer(double **matrix, unsigned int rows, unsigned int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i][j] = 0;
+        }
+    }
+    return;
+}
+
+double **matrix_allocation(unsigned int rows, unsigned int columns)
+{
+    double **matrix = (double **)malloc(rows * sizeof(double *));
+    for (int i = 0; i < rows; i++)
+        matrix[i] = (double *)malloc(columns * sizeof(double));
+    zero_matrix_initializer(matrix, rows, columns);
+    return matrix;
+}
 
 TEST_CASE("This is a test")
 {
-    REQUIRE(2 + 2 == 3);
+
+    unsigned int rows = 14;
+    unsigned int columns = 1;
+
+    double **matrix = matrix_allocation(rows, columns);
+
+    REQUIRE(2 + 2 == 4);
+    for (int i = 0; i < rows; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
 }
