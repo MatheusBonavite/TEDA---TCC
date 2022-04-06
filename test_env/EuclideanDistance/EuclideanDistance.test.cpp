1f86135d549a8dbfbb859a7181c338a062d24a2c
MatheusBonavite <matheusbonavite@ufmg.br>
2022-04-06 00:25:23 -0300

    TST002: successfully testing euclidean distance

--- test_env/EuclideanDistance/EuclideanDistance.test.cpp
+++ test_env/EuclideanDistance/EuclideanDistance.test.cpp
@@ -57,7 +57,7 @@ double squared_euclidean_distance(double **matrix, unsigned int i, unsigned int 
 
 TEST_CASE("One dimensional euclidean distance")
 {
-
+    unsigned int problem_dimension = 1;
     unsigned int rows = 14;
     unsigned int columns = 1;
     double test_1d[] = {
@@ -70,13 +70,66 @@ TEST_CASE("One dimensional euclidean distance")
         }
     }
     REQUIRE(
-        euclidean_distance(matrix, 0, 1, 1) == 17.2
+        fabs(euclidean_distance(matrix, 0, 1, problem_dimension) - 17.2) < 0.00001
+    );
+    REQUIRE(
+        fabs(euclidean_distance(matrix, 0, 2, problem_dimension) - 13.8) < 0.00001
+    );
+    REQUIRE(
+        fabs(euclidean_distance(matrix, 5, 10, problem_dimension) - 1.2) < 0.00001
+    );
+    REQUIRE(
+        fabs(euclidean_distance(matrix, 3, 13, problem_dimension) - 7.8) < 0.00001
+    );
+    dealloc_matrix(matrix, rows);
+}
+
+TEST_CASE("Two dimensional euclidean distance")
+{
+    unsigned int problem_dimension = 2;
+    unsigned int rows = 7;
+    unsigned int columns = 2;
+    double test_2d[7][2] = {
+        {20.2, 3.0}, {6.4, 11.6}, {8.2, 2.2}, {11.2, 5.2}, {6.2, 0.2}, {1.0, 4.8}, {2.4, 3.8}
+    };
+    double **matrix = matrix_allocation(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            matrix[i][j] = test_2d[i][j];
+        }
+    }
+    REQUIRE(
+        fabs(euclidean_distance(matrix, 0, 1, problem_dimension) - 16.260381) < 0.00001
+    );
+    REQUIRE(
+        fabs(euclidean_distance(matrix, 2, 6, problem_dimension) - 6.016644) < 0.00001
+    );
+    dealloc_matrix(matrix, rows);
+}
+
+TEST_CASE("Three dimensional euclidean distance")
+{
+    unsigned int problem_dimension = 3;
+    unsigned int rows = 4;
+    unsigned int columns = 3;
+    double test_2d[7][3] = {
+        {20.2, 3.0, 6.4}, {11.6, 8.2, 2.2}, {11.2, 5.2, 6.2}, {0.2, 1.0, 4.8}
+    };
+    double **matrix = matrix_allocation(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            matrix[i][j] = test_2d[i][j];
+        }
+    }
+    REQUIRE(
+        fabs(euclidean_distance(matrix, 0, 1, problem_dimension) - 10.892199) < 0.00001
     );
     dealloc_matrix(matrix, rows);
 }
 
 TEST_CASE("One dimensional squared euclidean distance")
 {
+    unsigned int problem_dimension = 1;
     unsigned int rows = 14;
     unsigned int columns = 1;
     double test_1d[] = {
@@ -89,7 +142,59 @@ TEST_CASE("One dimensional squared euclidean distance")
         }
     }
     REQUIRE(
-        squared_euclidean_distance(matrix, 0, 1, 1) == 295.84
+        fabs(squared_euclidean_distance(matrix, 0, 1, problem_dimension) -  295.84) < 0.00001
+    );
+    REQUIRE(
+        fabs(squared_euclidean_distance(matrix, 0, 2, problem_dimension) -  190.44) < 0.00001
+    );
+    REQUIRE(
+        fabs(squared_euclidean_distance(matrix, 5, 10, problem_dimension) - 1.44) < 0.00001
+    );
+    REQUIRE(
+        fabs(squared_euclidean_distance(matrix, 3, 13, problem_dimension) - 60.84) < 0.00001
+    );
+    dealloc_matrix(matrix, rows);
+}
+
+TEST_CASE("Two dimensional squared euclidean distance")
+{
+    unsigned int problem_dimension = 2;
+    unsigned int rows = 7;
+    unsigned int columns = 2;
+    double test_2d[7][2] = {
+        {20.2, 3.0}, {6.4, 11.6}, {8.2, 2.2}, {11.2, 5.2}, {6.2, 0.2}, {1.0, 4.8}, {2.4, 3.8}
+    };
+    double **matrix = matrix_allocation(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            matrix[i][j] = test_2d[i][j];
+        }
+    }
+    REQUIRE(
+        fabs(squared_euclidean_distance(matrix, 0, 1, problem_dimension) - 264.39999026516097) < 0.00001
+    );
+    REQUIRE(
+        fabs(squared_euclidean_distance(matrix, 2, 6, problem_dimension) - 36.200005022736) < 0.00001
+    );
+    dealloc_matrix(matrix, rows);
+}
+
+TEST_CASE("Three dimensional squared euclidean distance")
+{
+    unsigned int problem_dimension = 3;
+    unsigned int rows = 4;
+    unsigned int columns = 3;
+    double test_2d[7][3] = {
+        {20.2, 3.0, 6.4}, {11.6, 8.2, 2.2}, {11.2, 5.2, 6.2}, {0.2, 1.0, 4.8}
+    };
+    double **matrix = matrix_allocation(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            matrix[i][j] = test_2d[i][j];
+        }
+    }
+    REQUIRE(
+        fabs(squared_euclidean_distance(matrix, 0, 1, problem_dimension) - 118.639999055601) < 0.00001
     );
     dealloc_matrix(matrix, rows);
 }
