22ab8bdb17da9e06d388986e0fd5c99848f8f747
MatheusBonavite <matheusbonavite@ufmg.br>
2022-04-10 12:17:32 -0300

    TST006 > Calculating online eccentricity and testing it

--- test_env/OnlineEccentricity/OnlineEccentricity.test.cpp
+++ test_env/OnlineEccentricity/OnlineEccentricity.test.cpp
@@ -43,22 +43,83 @@ double cumulative_proximity(double *matrix, unsigned int i, unsigned int rows, u
     return result;
 }
 
+void update_mi_current(double *mi_current, double *sample_current, unsigned int matrix_index, unsigned int columns){
+    if(matrix_index == 0){
+        for(unsigned int j=0; j < columns; j++){
+            mi_current[j] = sample_current[j];
+        }
+        return;
+    }
+    double first_fraction = (double) (matrix_index)/(matrix_index + 1.0);
+    double second_fraction = (double) (1.0)/(matrix_index + 1.0);
+    for(unsigned int j=0; j<columns; j++){
+        mi_current[j] = (first_fraction*mi_current[j]) + (second_fraction*sample_current[j]);
+    }
+    return;
+}
+
+double vec_dot_product(double *vector_a, double *vector_b, unsigned int columns){
+    double *aux = (double *) malloc(columns * sizeof(double));
+    double dot_product = 0.0;
+    for(unsigned int j=0; j<columns; j++){
+        aux[j] = (vector_a[j] - vector_b[j]);
+        dot_product = dot_product + (aux[j] * aux[j]);
+    }
+    free(aux);
+    return dot_product;
+}
+
+void update_sigma_current(double *sigma_current, double *mi_current, double *sample_current, unsigned int matrix_index, unsigned int columns){
+    if(matrix_index == 0){
+        *sigma_current = 0.0;
+        return;
+    }
+    double first_fraction = (double) (matrix_index)/(matrix_index + 1.0);
+    double second_fraction = (double) (1.0)/(matrix_index);
+    double dot_product = vec_dot_product(mi_current, sample_current, columns);
+    *sigma_current = (first_fraction*(*sigma_current)) + (second_fraction*dot_product);
+    return;
+}
+
 double *online_eccentricity_s(
     double *matrix,
     unsigned int matrix_index,
     double *sample_current,
+    double *mi_current,
+    double *sigma_current,
+    double *eccentricity,
     unsigned int columns
 ){
-
     for(unsigned int j=0; j<columns; j++){
         matrix[(columns * matrix_index) + j] = sample_current[j];
     }
+
     double* matrix_aux = (double *) realloc(matrix, (((matrix_index+2)*columns) * sizeof(double)));
     if(matrix_aux == NULL){
         printf("Not enough memory \n");
         free(matrix);
         exit(1);
     }
+
+    update_mi_current(mi_current, sample_current, matrix_index, columns);
+    update_sigma_current(sigma_current, mi_current, sample_current, matrix_index, columns);
+
+    if(*sigma_current == 0.0){
+        printf("Matrix index ::: %i \n", matrix_index);
+        printf("Sigma value ::: %lf \n", *sigma_current);
+        *eccentricity = (double) (1.0/(matrix_index+1.0));
+        return matrix_aux;
+    }
+
+    printf("Matrix index ::: %i \n", matrix_index);
+    printf("Sigma value ::: %lf \n", *sigma_current);
+    for(unsigned int j=0; j<columns; j++){
+        printf("Mi[%i] value :::: %lf \n", j, mi_current[j]);
+    }
+
+    double dot_product = vec_dot_product(mi_current, sample_current, columns);
+    double denominator = (double) (matrix_index+1.0) * (*sigma_current);
+    *eccentricity = (1/(matrix_index + 1.0)) + (dot_product/denominator);
     return matrix_aux;
 }
 
@@ -67,14 +128,22 @@ TEST_CASE("One dimensional online eccentricity proximity")
     unsigned int rows = 4;
     unsigned int columns = 1;
     unsigned int index = 0;
-    double test_1d[rows][columns] = { { 20.0 }, { 12.0 }, { 10.0 }, { 11.0 } };
+    double sigma_current_value = 0.0;
+    double eccentricity_value = 1.0;
+    double test_1d[rows][columns] = { { 20.0 }, { 12.0 }, { 10.0 } };
 
     double *matrix = matrix_allocation(1, columns);
+    double *mi_current = (double *) malloc(columns * sizeof(double));
+    double *sigma_current = &sigma_current_value;
+    double *eccentricity = &eccentricity_value;
+
     for(unsigned int i=0; i<rows; i++){
-        matrix = online_eccentricity_s(matrix, index, test_1d[i], columns);
+        matrix = online_eccentricity_s(matrix, index, test_1d[i], mi_current, sigma_current, eccentricity, columns);
+        printf("The eccentricity value is: %lf \n", *eccentricity);
         index++;
     }
     free(matrix);
+    free(mi_current);
     REQUIRE(
         0 == 0
     );
