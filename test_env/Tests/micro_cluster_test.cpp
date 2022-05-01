2226fcaac7d03c9e87275990c5ac264bb2f78e8a
Matheus Bonavite <matheusbonavite@gmail.com>
2022-05-01 13:58:52 -0300

    TST020: adding more tests for micro_cluster (2D)

--- test_env/Tests/micro_cluster_test.cpp
+++ test_env/Tests/micro_cluster_test.cpp
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 #include "./header/global_header.h"
 
-TEST_CASE("Micro cluster call")
+TEST_CASE("Micro cluster call - one dimensional")
 {
     unsigned int samples = 1;
     unsigned int n = 0;
     unsigned int columns = 1;
     unsigned int rows = 4;
-    double test_1d[4][1] = {{20.0}, {19.99}, {12.0}, {11.99}};
+    double test_1d[4][1] = {{20.0}, {19.9}, {19.95}, {11.99}};
     struct Micro_Cluster *micro_clusters_arr;
     unsigned int *number_of_micro_clusters = &n;
     for (unsigned int i = 0; i < rows; i++)
@@ -18,11 +18,47 @@ TEST_CASE("Micro cluster call")
         micro_clusters_arr = update_micro_cluster(micro_clusters_arr, number_of_micro_clusters, test_1d[i], i, columns);
     }
     printf("Number of micro_clusters are :::: %u\n", *number_of_micro_clusters);
-    for(unsigned int i=0; i < *number_of_micro_clusters; i++){
+    for (unsigned int i = 0; i < *number_of_micro_clusters; i++)
+    {
         printf("\n1) Micro[%u].Variance ::: %lf\n", i, micro_clusters_arr[i].variance);
-        printf("\n2) Micro[%u].Eccentricity ::: %lf\n",  i, micro_clusters_arr[i].eccentricity);
+        printf("\n2) Micro[%u].Eccentricity ::: %lf\n", i, micro_clusters_arr[i].eccentricity);
         printf("\n3) Micro[%u].Center ::: \n", i);
-        for(unsigned int j=0; j<columns; j++){
+        for (unsigned int j = 0; j < columns; j++)
+        {
+            printf("Micro[%u].Center ::: %lf\n", i, micro_clusters_arr[i].center[j]);
+        }
+        printf("===============================================================================\n");
+    }
+    for (unsigned int j = 0; j < *number_of_micro_clusters; j++)
+    {
+        free(micro_clusters_arr[j].center);
+    }
+    REQUIRE(0 == 0);
+}
+
+TEST_CASE("Micro cluster call - two dimensional")
+{
+    unsigned int problem_dimension = 2;
+    unsigned int rows = 7;
+    unsigned int columns = 2;
+    unsigned int n = 0;
+    double test_2d[7][2] = {
+        {20.2, 3.0}, {6.4, 11.6}, {8.2, 2.2}, {11.2, 5.2}, {6.2, 0.2}, {1.0, 4.8}, {2.4, 3.8}};
+    double *matrix = matrix_allocation(rows, columns);
+    struct Micro_Cluster *micro_clusters_arr;
+    unsigned int *number_of_micro_clusters = &n;
+    for (unsigned int i = 0; i < rows; i++)
+    {
+        micro_clusters_arr = update_micro_cluster(micro_clusters_arr, number_of_micro_clusters, test_2d[i], i, columns);
+    }
+    printf("Number of micro_clusters are :::: %u\n", *number_of_micro_clusters);
+    for (unsigned int i = 0; i < *number_of_micro_clusters; i++)
+    {
+        printf("\n1) Micro[%u].Variance ::: %lf\n", i, micro_clusters_arr[i].variance);
+        printf("\n2) Micro[%u].Eccentricity ::: %lf\n", i, micro_clusters_arr[i].eccentricity);
+        printf("\n3) Micro[%u].Center ::: \n", i);
+        for (unsigned int j = 0; j < columns; j++)
+        {
             printf("Micro[%u].Center ::: %lf\n", i, micro_clusters_arr[i].center[j]);
         }
         printf("===============================================================================\n");
