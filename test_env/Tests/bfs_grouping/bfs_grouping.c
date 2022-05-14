5b71bba68fd3da5588901d90cd13064f057ad0c3
Matheus Bonavite <matheusbonavite@gmail.com>
2022-05-14 15:11:48 -0300

    TST042: bfs grouping test adding

---
+++ test_env/Tests/bfs_grouping/bfs_grouping.c
@@ -0,0 +1,343 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Macro_Clusters
+{
+    unsigned int *group_of_micro_clusters;
+    unsigned int n_micro_clusters;
+    double micro_density_mean;
+};
+
+struct Micro_Cluster
+{
+    unsigned int number_of_data_samples;
+    double *center;
+    double variance;
+    double eccentricity;
+    double typicality;
+    double density;
+    double outlier_threshold_parameter;
+};
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double vec_dot_product(double *vector_a, double *vector_b, unsigned int columns)
+{
+    double *aux = (double *)malloc(columns * sizeof(double));
+    double dot_product = 0.0;
+    for (unsigned int j = 0; j < columns; j++)
+    {
+        aux[j] = (vector_a[j] - vector_b[j]);
+        dot_product = dot_product + (aux[j] * aux[j]);
+    }
+    free(aux);
+    return dot_product;
+}
+
+void recursive_biased_sigma(double *sigma_current, double *mi_current, double *sample_current, unsigned int matrix_index, unsigned int columns)
+{
+    if (matrix_index == 0)
+    {
+        *sigma_current = 0.0;
+        return;
+    }
+    double first_fraction = (double)(matrix_index) / (matrix_index + 1.0);
+    double second_fraction = (double)(1.0) / (matrix_index);
+    double dot_product = vec_dot_product(mi_current, sample_current, columns);
+    *sigma_current = (first_fraction * (*sigma_current)) + (second_fraction * dot_product);
+    return;
+}
+
+void recursive_mean(double *mi_current, double *sample_current, unsigned int matrix_index, unsigned int columns)
+{
+    if (matrix_index == 0)
+    {
+        for (unsigned int j = 0; j < columns; j++)
+        {
+            mi_current[j] = sample_current[j];
+        }
+        return;
+    }
+    double first_fraction = (double)(matrix_index) / (matrix_index + 1.0);
+    double second_fraction = (double)(1.0) / (matrix_index + 1.0);
+    for (unsigned int j = 0; j < columns; j++)
+    {
+        mi_current[j] = (first_fraction * mi_current[j]) + (second_fraction * sample_current[j]);
+    }
+    return;
+}
+
+void recursive_eccentricity(
+    unsigned int matrix_index,
+    double *sample_current,
+    double *mi_current,
+    double *sigma_current,
+    double *eccentricity,
+    unsigned int columns)
+{
+    recursive_mean(mi_current, sample_current, matrix_index, columns);
+    recursive_biased_sigma(sigma_current, mi_current, sample_current, matrix_index, columns);
+    double dot_product = vec_dot_product(sample_current, mi_current, columns);
+    double denominator = (double)(matrix_index + 1.0) * (*sigma_current);
+    *eccentricity = (1.0 / (matrix_index + 1.0)) + (dot_product / denominator);
+    return;
+}
+
+double two_vec_euclidean_distance(double *a, double *b, unsigned int columns)
+{
+    double result = 0.0;
+    for (int w = 0; w < columns; w++)
+    {
+        result += ((a[w] - b[w]) * (a[w] - b[w]));
+    }
+
+    return (double)sqrt(result);
+}
+
+void int_zero_matrix_initializer(unsigned int *matrix, unsigned int rows, unsigned int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[(columns * i) + j] = 0;
+        }
+    }
+    return;
+}
+
+double empirical_m(int k)
+{
+    double denominator = exp(-0.007 * (k - 100.0));
+    return (3.0 / (1.0 + denominator));
+}
+
+void adjency_matrix(struct Micro_Cluster *micro_clusters_arr, unsigned int *adj_nodes, unsigned int number_of_micro_clusters, unsigned int columns)
+{
+    double dist_centers = 0.0;
+    double variance_sum = 0.0;
+    for (unsigned int i = 0; i < number_of_micro_clusters; i++)
+    {
+        for (unsigned int j = 0; j < number_of_micro_clusters; j++)
+        {
+            if (j <= i)
+            {
+                continue;
+            }
+            dist_centers = two_vec_euclidean_distance(micro_clusters_arr[i].center, micro_clusters_arr[j].center, columns);
+
+            if (micro_clusters_arr[i].variance > 0.0 && micro_clusters_arr[j].variance > 0.0)
+            {
+                variance_sum = 2.0 * (sqrt(micro_clusters_arr[i].variance) + sqrt(micro_clusters_arr[j].variance));
+                unsigned int resp_condition = dist_centers < variance_sum;
+                adj_nodes[(number_of_micro_clusters * i) + j] = resp_condition;
+                adj_nodes[(number_of_micro_clusters * j) + i] = resp_condition;
+            }
+            else
+            {
+                if (micro_clusters_arr[i].variance > 0.0)
+                {
+                    double i_radius = empirical_m(micro_clusters_arr[i].number_of_data_samples) * sqrt(micro_clusters_arr[i].variance);
+                    unsigned int resp_condition = dist_centers <= i_radius;
+                    adj_nodes[(number_of_micro_clusters * i) + j] = resp_condition;
+                    adj_nodes[(number_of_micro_clusters * j) + i] = resp_condition;
+                }
+                else
+                {
+                    double j_radius = empirical_m(micro_clusters_arr[j].number_of_data_samples) * sqrt(micro_clusters_arr[j].variance);
+                    unsigned int resp_condition = dist_centers <= j_radius;
+                    adj_nodes[(number_of_micro_clusters * i) + j] = resp_condition;
+                    adj_nodes[(number_of_micro_clusters * j) + i] = resp_condition;
+                }
+            }
+        }
+    }
+    return;
+}
+struct Micro_Cluster *allocate_initial_micro_cluster(unsigned int *number_of_micro_clusters, double *sample_current, unsigned int columns)
+{
+    struct Micro_Cluster *temp = (struct Micro_Cluster *)calloc(1, sizeof(struct Micro_Cluster));
+    if (temp == NULL)
+    {
+        printf("Cannot allocate initial memory.\n");
+        exit(1);
+    }
+    else
+    {
+        (*number_of_micro_clusters) = 1;
+        temp[0].number_of_data_samples = 1;
+        temp[0].center = (double *)malloc(columns * sizeof(double));
+        for (unsigned int i = 0; i < columns; i++)
+        {
+            temp[0].center[i] = sample_current[i];
+        }
+        temp[0].variance = 0.0;
+        return temp;
+    }
+}
+
+struct Micro_Cluster *update_micro_cluster(struct Micro_Cluster *micro_clusters_arr, unsigned int *number_of_micro_clusters, double *sample_current, unsigned int k, unsigned int columns)
+{
+    double r_0 = 0.001;
+    if ((k + 1) == 1)
+    {
+        return allocate_initial_micro_cluster(number_of_micro_clusters, sample_current, columns);
+    }
+    else
+    {
+        unsigned int flag = 1;
+        for (unsigned int i = 0; i < (*number_of_micro_clusters); i++)
+        {
+            unsigned int outlier = 1;
+            struct Micro_Cluster temp;
+            temp.number_of_data_samples = micro_clusters_arr[i].number_of_data_samples;
+            temp.variance = micro_clusters_arr[i].variance;
+            temp.eccentricity = micro_clusters_arr[i].eccentricity;
+            temp.center = (double *)malloc(columns * sizeof(double));
+            for (unsigned int j = 0; j < columns; j++)
+            {
+                temp.center[j] = micro_clusters_arr[i].center[j];
+            }
+            recursive_eccentricity(temp.number_of_data_samples, sample_current, temp.center, &temp.variance, &temp.eccentricity, columns);
+            if (temp.number_of_data_samples == 1)
+            {
+                temp.outlier_threshold_parameter = empirical_m(2);
+                double squared_threshold = temp.outlier_threshold_parameter * temp.outlier_threshold_parameter;
+                int first_condition = (temp.eccentricity / 2.0) > ((squared_threshold + 1.0) / 4.0);
+                int second_condition = temp.variance > r_0;
+                outlier = first_condition || second_condition;
+            }
+            else
+            {
+                temp.outlier_threshold_parameter = empirical_m(temp.number_of_data_samples + 1.0);
+
+                double squared_threshold = temp.outlier_threshold_parameter * temp.outlier_threshold_parameter;
+
+                outlier = (temp.eccentricity / 2.0) > (((squared_threshold + 1.0) / (2.0 * (temp.number_of_data_samples + 1.0))));
+            }
+
+            if (outlier == 0)
+            {
+                flag = 0;
+                micro_clusters_arr[i].variance = temp.variance;
+                micro_clusters_arr[i].eccentricity = temp.eccentricity;
+                micro_clusters_arr[i].number_of_data_samples = temp.number_of_data_samples + 1.0;
+                for (unsigned int j = 0; j < columns; j++)
+                {
+                    micro_clusters_arr[i].center[j] = temp.center[j];
+                }
+            }
+            free(temp.center);
+        }
+        if (flag == 1)
+        {
+            (*number_of_micro_clusters) += 1;
+            struct Micro_Cluster *new_micro_arr = (struct Micro_Cluster *)realloc(micro_clusters_arr, (*number_of_micro_clusters) * sizeof(struct Micro_Cluster));
+            if (new_micro_arr == NULL)
+            {
+                printf("Cannot allocate more memory.\n");
+                exit(1);
+            }
+            micro_clusters_arr = new_micro_arr;
+            micro_clusters_arr[(*number_of_micro_clusters) - 1].number_of_data_samples = 1;
+            micro_clusters_arr[(*number_of_micro_clusters) - 1].center = (double *)malloc(columns * sizeof(double));
+            for (unsigned int i = 0; i < columns; i++)
+            {
+                micro_clusters_arr[(*number_of_micro_clusters) - 1].center[i] = sample_current[i];
+            }
+            micro_clusters_arr[(*number_of_micro_clusters) - 1].variance = 0.0;
+        }
+    }
+    return micro_clusters_arr;
+}
+
+struct Macro_Clusters *bfs_grouping(struct Macro_Clusters *macro_clusters_arr, struct Micro_Cluster *micro_clusters_arr, unsigned int *adjency_matrix, unsigned int *number_of_macro_clusters, unsigned int number_of_micro_clusters)
+{
+    unsigned int *visited = (unsigned int *)calloc((number_of_micro_clusters), sizeof(unsigned int));
+    for (unsigned int w = 0; w < number_of_micro_clusters; w++)
+    {
+        unsigned int start_point = w;
+        if (visited[start_point] == 1)
+            continue;
+
+        unsigned int *queue = (unsigned int *)calloc((number_of_micro_clusters), sizeof(unsigned int));
+        unsigned int front = 0, rear = 0;
+        queue[rear] = start_point;
+        visited[start_point] = 1;
+
+        // when front > rear, means queue is empty!
+        while (front <= rear)
+        {
+            start_point = queue[front];
+            for (unsigned int i = 0; i < number_of_micro_clusters; i++)
+            {
+                if (adjency_matrix[(start_point * number_of_micro_clusters) + i] == 1 && visited[i] == 0)
+                {
+                    queue[++rear] = i;
+                    visited[i] = 1;
+                }
+            }
+            front++;
+        }
+        if (rear == 0) // To avoid having a macro-cluster with only one micro-cluster
+            continue;
+        if (*number_of_macro_clusters == 0)
+        {
+            macro_clusters_arr = (struct Macro_Clusters *)calloc(1, sizeof(struct Macro_Clusters));
+            if (macro_clusters_arr == NULL)
+            {
+                printf("Could not allocate memory \n");
+                exit(1);
+            }
+        }
+        else
+        {
+            struct Macro_Clusters *new_macro_arr = (struct Macro_Clusters *)realloc(macro_clusters_arr, (*number_of_macro_clusters + 1) * sizeof(struct Macro_Clusters));
+            if (new_macro_arr == NULL)
+            {
+                printf("Cannot allocate more memory.\n");
+                exit(1);
+            }
+            macro_clusters_arr = new_macro_arr;
+        }
+        double density_mean = 0.0;
+        unsigned int computing_density_mean = 0;
+        double *current_density_mean = &density_mean;
+        macro_clusters_arr[*number_of_macro_clusters].group_of_micro_clusters = (unsigned int *)calloc(rear + 1, sizeof(unsigned int));
+        macro_clusters_arr[*number_of_macro_clusters].n_micro_clusters = rear + 1;
+
+        if (macro_clusters_arr[*number_of_macro_clusters].group_of_micro_clusters == NULL)
+        {
+            printf("Could not allocate memory \n");
+            exit(1);
+        }
+        for (unsigned int wu = 0; wu < rear; wu++)
+        {
+            unsigned int micro_index = queue[wu];
+            macro_clusters_arr[*number_of_macro_clusters].group_of_micro_clusters[wu] = micro_index;
+            if (micro_clusters_arr[micro_index].eccentricity > 0.000001)
+            {
+                double density = 2.0 / micro_clusters_arr[micro_index].eccentricity;
+                double *current_density = &density;
+                recursive_mean(current_density_mean, current_density, computing_density_mean, 1);
+                computing_density_mean++;
+            }
+        }
+        macro_clusters_arr[*number_of_macro_clusters].group_of_micro_clusters[rear] = start_point;
+
+        if (micro_clusters_arr[start_point].eccentricity > 0.000001)
+        {
+            double density = 2.0 / micro_clusters_arr[start_point].eccentricity;
+            double *current_density = &density;
+            recursive_mean(current_density_mean, current_density, computing_density_mean, 1);
+        }
+        macro_clusters_arr[*number_of_macro_clusters].micro_density_mean = *current_density_mean;
+        *number_of_macro_clusters = *number_of_macro_clusters + 1;
+
+        free(queue);
+    }
+    free(visited);
+    return macro_clusters_arr;
+}
---
+++ test_env/Tests/bfs_grouping/build.test.sh
@@ -0,0 +1,4 @@
+g++ -std=c++11 -w bfs_grouping.c bfs_grouping_test.cpp -o bfs_grouping_test
+./bfs_grouping_test --list-tests
+./bfs_grouping_test
+rm -f bfs_grouping_test
