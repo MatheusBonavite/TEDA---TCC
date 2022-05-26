0051365b1ccf652eb0c650280e81dfc0a9fe4c53
Matheus Bonavite <matheusbonavite@gmail.com>
2022-05-26 20:18:26 -0300

    dev(recursive_eccentricity): changing a missing guard

--- Dev/Functions/MathFunctions/recursive_eccentricity.c
+++ Dev/Functions/MathFunctions/recursive_eccentricity.c
@@ -1,3 +1,5 @@
+#define EPS (1e-10)
+
 void recursive_mean(double *mi_current, double *sample_current, unsigned int matrix_index, unsigned int columns);
 void recursive_biased_sigma(double *sigma_current, double *mi_current, double *sample_current, unsigned int matrix_index, unsigned int columns);
 double vec_dot_product(double *vector_a, double *vector_b, unsigned int columns);
@@ -38,9 +40,10 @@ void recursive_eccentricity_guarded(
         *eccentricity = 1000.0;
         return;
     }
-    if (*sigma_current <= 0.000001 && (matrix_index + 1) > 1)
+    if (*sigma_current <= EPS && (matrix_index + 1) > 1)
     {
         *eccentricity = (double)(1.0 / (matrix_index + 1.0));
+        return;
     }
     *eccentricity = (1.0 / (matrix_index + 1.0)) + (dot_product / denominator);
     return;
